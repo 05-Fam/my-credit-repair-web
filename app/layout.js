32077637350f1dc1e1e63caf844f3bc78d970a3f
05-Fam <tm84like@hotmail.com>
2025-07-31 17:17:54 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,9 @@
 // app/layout.js
+export const metadata = {
+  title: "My Credit Repair Web",
+  description: "A simple landing page built with Next.js 14",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
